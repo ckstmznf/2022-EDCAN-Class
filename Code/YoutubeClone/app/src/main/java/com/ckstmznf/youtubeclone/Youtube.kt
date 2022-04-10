d7f37dc366684797be73ca26ad36d0d5434c53fe
ckstmznf <ckstmznf@naver.com>
2022-04-10 20:53:57 +0900

    0410 1차 백업
    - Android 기초 수업
    - Firebase 수업(Auth, Firestore)

---
+++ Code/YoutubeClone/app/src/main/java/com/ckstmznf/youtubeclone/Youtube.kt
@@ -0,0 +1,10 @@
+package com.ckstmznf.youtubeclone
+
+data class Youtube(
+    val title : String,
+    val thumbnail : String,
+    val profileImg : String,
+    val channelName : String,
+    val viewCount : Int,
+    val url : String,
+)
---
+++ Code/YoutubeClone/app/src/main/java/com/ckstmznf/youtubeclone/YoutubeAdapter.kt
@@ -0,0 +1,19 @@
+package com.ckstmznf.youtubeclone
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.ckstmznf.youtubeclone.R
+
+class YoutubeAdapter(val datas : List<Youtube>) : RecyclerView.Adapter<YoutubeListViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): YoutubeListViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.row_youtube_list_item, parent, false)
+        return YoutubeListViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: YoutubeListViewHolder, position: Int) {
+        holder.onBind(datas[position])
+    }
+
+    override fun getItemCount(): Int = datas.size
+}
